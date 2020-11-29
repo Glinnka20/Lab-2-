b52841723575bc17544135dd6b18c64bc47e6749
NikitkaGL <75216078+NikitkaGL@users.noreply.github.com>
2020-11-29 22:01:04 +0200

    Lab(2)
    Lab(2)

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ООП_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            RegularHexagon st1 = new RegularHexagon();
+            st1.radiys(); st1.ploshy();
+            st1.rezzult();
+        }
+        
+    }
+}
+
---
+++ RegularHexagon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ООП_2_
+{
+    class RegularHexagon
+    {
+
+        string rad;
+      public  double rad1 { get; set; }
+      public double  ploshy1 { get; set; }
+        // Конструкор 
+        public void radiys()
+        {
+            Console.WriteLine("Введите радиус вписанной окружности");
+            rad = Console.ReadLine();
+            rad1 = Convert.ToDouble(rad);
+        }
+        public void ploshy()
+        {
+            ploshy1 = 2 * Math.Sqrt(3) * Math.Pow(rad1, 2);
+        }
+        public void rezzult()
+        {
+            Console.WriteLine("Площадь правильного шестиугольника=" + ploshy1);
+        }
+        //Деструктор
+        ~RegularHexagon()
+        {
+            Console.WriteLine("Dispose object...");
+        }
+    }
+
+}
